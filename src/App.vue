96567af064c6f081d633573ac73a67aef8c857bd
jialiangzouMBP <1214311807@qq.com>
2022-10-15 22:29:10 +0800

    fix:App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
+<!--
+ * @Author: jialiangzou
+ * @Date: 2022-10-13 20:38:37
+ * @LastEditors: jialiangzou
+ * @LastEditTime: 2022-10-15 22:28:36
+ * @FilePath: /lowcode-web/src/App.vue
+-->
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
