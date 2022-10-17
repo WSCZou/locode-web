6d0cb5cb8c23a78a92e48f2289ca5c940060a5ac
jialiangzouMBP <1214311807@qq.com>
2022-10-17 22:16:32 +0800

    fix:eslint报错

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,19 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+/*
+ * @Author: jialiangzou
+ * @Date: 2022-10-13 20:38:37
+ * @LastEditors: jialiangzou
+ * @LastEditTime: 2022-10-17 22:16:13
+ * @FilePath: /lowcode-web/src/stores/counter.ts
+ */
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const doubleCount = computed(() => count.value * 2);
   function increment() {
-    count.value++
+    count.value++;
   }
 
-  return { count, doubleCount, increment }
-})
+  return { count, doubleCount, increment };
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Author: jialiangzou
+ * @Date: 2022-10-13 20:38:37
+ * @LastEditors: jialiangzou
+ * @LastEditTime: 2022-10-17 22:16:07
+ * @FilePath: /lowcode-web/src/views/HomeView.vue
+-->
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
